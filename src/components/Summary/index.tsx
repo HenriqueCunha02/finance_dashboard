382e493d846c01ecf636c5b4634235a77d4923cc
Henrique Cunha <henriqueartusi39@gmail.com>
2026-09-03 18:18:03 -0300

    feat: add transition form

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -7,43 +7,34 @@ export function Summary() {
     <div className={styles.container}>
       <div className={styles.summary}>
         <span className={styles.title}>Saldo</span>
-
         <div className={styles.iconAmount}>
           <div className={styles.iconContainer}>
-            <Wallet className={styles.wallet} color='#22c55e' />
+            <Wallet className={styles.wallet} />
           </div>
-
           <span className={styles.amount}>R$2900,00</span>
         </div>
-
         <p className={styles.footer}>Total disponível</p>
       </div>
 
       <div className={styles.summary}>
         <span className={styles.title}>Receitas</span>
-
         <div className={styles.iconAmount}>
           <div className={styles.iconContainer}>
-            <ArrowDownToLine className={styles.wallet} color='#22c55e' />
+            <ArrowDownToLine className={styles.wallet} />
           </div>
-
           <span className={styles.amount}>R$3000,00</span>
         </div>
-
         <p className={styles.footer}>Total de entradas</p>
       </div>
 
       <div className={styles.summary}>
         <span className={styles.title}>Despesas</span>
-
         <div className={styles.iconAmount}>
           <div className={styles.iconContainer}>
             <ArrowDownFromLine className={styles.arrowDownFromLine} />
           </div>
-
           <span className={styles.spent}>R$100,00</span>
         </div>
-
         <p className={styles.footer}>Total de saídas</p>
       </div>
     </div>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
+import { TransitionForm } from '../../components/TransitionForm';
 
 export function Home() {
   return (
     <>
       <Header />
       <Summary />
+      <TransitionForm />
     </>
   );
 }
